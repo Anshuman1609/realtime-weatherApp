0f487912e955960ee05ae7cf5bbf2a1ce8ea1709
Anshuman Dubey <anshumandubey1609@gmail.com>
2023-11-29 23:21:58 +0530

    new commit

--- src/components/weather/temp.js
+++ src/components/weather/temp.js
@@ -1,9 +1,14 @@
+//https://api.openweathermap.org/data/2.5/weather?q=Ghaziabad&appid=136cd51b0de5bfcb459f39001feb2d22
 import React from 'react';
 
 const temp = () => {
-  return (
-    <div>temp</div>
-  )
+  return <>
+    <div className="wrap">
+      <div className="search">
+
+      </div>
+    </div>
+  </>
 }
 
 export default temp;
