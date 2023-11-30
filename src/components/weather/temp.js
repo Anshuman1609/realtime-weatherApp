e06e34e3a158ff4336ebfe38f45a2fb5da2d40cb
Anshuman Dubey <anshumandubey1609@gmail.com>
2023-11-30 10:09:25 +0530

    added all functionalities

--- src/components/weather/temp.js
+++ src/components/weather/temp.js
@@ -1,80 +1,57 @@
-//https://api.openweathermap.org/data/2.5/weather?q=Ghaziabad&appid=136cd51b0de5bfcb459f39001feb2d22
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./style.css";
+import Weathercard from './weathercard';
 
-const temp = () => {
+const Temp = () => {
 
-  
+  const [searchValue, setSearchValue] = useState("Delhi");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try{
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=136cd51b0de5bfcb459f39001feb2d22`;
+      const res = await fetch(url);
+      const data = await res.json();
+      
+      const { temp, humidity, pressure } = data.main;
+      const {main: weathermood} = data.weather[0];
+      const {name} = data;
+      const {speed} = data.wind;
+      const {country, sunset} = data.sys;
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    }
+    catch(error){
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  },);
 
   return <>
     <div className="wrap">
       <div className="search">
-        <input type="search" placeholder="search..." autoFocus id="search" className="searchTerm" />
+        <input type="search" placeholder="search..." autoFocus id="search" className="searchTerm" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />                                        
         <button className="searchButton" type="button" onClick={getWeatherInfo}>
           Search
         </button>
       </div>
     </div>
     {/* our temp card */}
-    <article className="widget">
-      <div className="weatherIcon">
-        <i className={"wi wi-day-sunny"}></i>
-      </div>
-      <div className="weatherInfo">
-        <div className="temperature">
-          <span>25.5&deg;</span>
-        </div>
-        <div className="description">
-          <div className="weatherCondition">sunny</div>
-          <div className="place">pune, India</div>
-        </div>
-      </div>
-      <div className="date">{ new Date().toLocaleString() } </div>
-      {/* our 4 column section */}
-      <div className="extra-temp">
-        <div className="temp-info-minmax">
-          <div className="two-sided-section">
-            <p>
-              <i className={"wi wi-sunset"}></i>
-            </p>
-            <p className="extra-info-leftside">
-              19:19 PM <br/>
-              Sunset
-            </p>
-          </div>
-          <div className="two-sided-section">
-            <p>
-              <i className={"wi wi-humidity"}></i>
-            </p>
-            <p className="extra-info-leftside">
-              19:19 PM <br/>
-              Humidity
-            </p>
-          </div>
-        </div>
-        <div className="weather-extra-info">
-        <div className="two-sided-section">
-            <p>
-              <i className={"wi wi-rain"}></i>
-            </p>
-            <p className="extra-info-leftside">
-              19:19 PM <br/>
-              Pressure
-            </p>
-          </div>
-          <div className="two-sided-section">
-            <p>
-              <i className={"wi wi-strong-wind"}></i>
-            </p>
-            <p className="extra-info-leftside">
-              19:19 PM <br/>
-              Speed
-            </p>
-          </div>
-        </div>
-      </div>
-    </article>
+    <Weathercard tempInfo = {tempInfo} />
   </>
 }
 
-export default temp;
+export default Temp;
